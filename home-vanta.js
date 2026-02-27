bedb650fe7b41efd903b8205ed21a4da6f3fe974
Neil Dandekar <neildandekar10@gmail.com>
2026-02-27 15:00:44 -0800

    chore(cleanup): remove obsolete legacy runtime artifacts

--- home-vanta.js
+++
@@ -1,6 +0,0 @@
-(() => {
-    const s = document.createElement('script');
-    s.src = '/app/legacy/home-vanta.js';
-    s.defer = true;
-    document.head.appendChild(s);
-})();
--- layout.js
+++
@@ -1,6 +0,0 @@
-(() => {
-    const s = document.createElement('script');
-    s.src = '/app/core/layout.js';
-    s.defer = true;
-    document.head.appendChild(s);
-})();
